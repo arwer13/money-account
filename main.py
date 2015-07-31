300edd7f1a6eb3f5211e2ef963ca20a8e3c5d7cd
Artyom Veremeenko <arwer13@gmail.com>
2015-07-31 08:20:02 +0300

    Just a bit more code.

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 import re
 
+
+
+
 def parse_line(s):
-    r = re.compile(r' *(\d\d\d\d\.\d\d\.\d\d) *(.*) +([\d\+-\.,]+) *')
-    date_gr, desc_gr, count_gr = r.match(s).groups()
-    return date_gr, desc_gr, count_gr
+    r = re.compile(r' *(\d\d\d\d\.\d\d\.\d\d) *([^;]+);?([^;]*) +([\d\+-\.,]+) *')
+    date_gr, cat_gr, tag_gr, count_gr = r.match(s).groups()
+    return date_gr, cat_gr, tag_gr, count_gr
     # return None
+
+
+class Entry:
+    def __init__(self):
+        self.categories = None
+        self.tags = None
+        self.time = None
+        self.increment = None
+
+    def __init__(self, s):
+        r = re.compile(r' *(\d\d\d\d\.\d\d\.\d\d) *(.*) +([\d\+-\.,]+) *')
+        date_gr, desc_gr, count_gr = r.match(s).groups()
+        pass
+
+    def __str__(self):
+        return ''.format(self.category)
+
 
 
 if __name__ == "__main__":
--- main_test.py
+++ main_test.py
@@ -3,22 +3,13 @@ import unittest
 import main
 
 
-class Entry:
-    def __init__(self):
-        self.category = None
-        self.tags = None
-        self.time = None
-        self.increment = None
-
-
 class TestSomething(unittest.TestCase):
 
     def test_parse_line(self):
         lines = [
-                "2015.07.09   food; tinned 67+77+51+41+57+36+40",
+                "2015.07.09   food; tinned food 67+77+51+41+57+36+40",
                 "2015.07.08 household      34",
-                " 2015.07.08 misc;  +500 ",
-                "2015.07.08 34"
+                " 2015.07.08 misc;  +500 "
         ]
         for s in lines:
             print(main.parse_line(s))
