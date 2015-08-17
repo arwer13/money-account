3e238ff5e865acbf5edc8aa583f0bb9dd3afcc40
Artyom Veremeenko <arwer13@gmail.com>
2015-08-18 00:22:16 +0300

    Able to process all lines. Count total value.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import re
-
+import datetime
 
 
 
@@ -12,21 +12,64 @@ def parse_line(s):
 
 
 class Entry:
-    def __init__(self):
-        self.categories = None
+
+    def __init__(self, s=None):
+        self.cats = None
         self.tags = None
         self.time = None
-        self.increment = None
+        self.value = None
+        if s is None:
+            return
+        if s.strip() == "" or s.strip()[0] == '#':
+            return
+        r = re.compile(r' *(\d\d\d\d\.\d\d\.\d\d) *(.*) +([\(\)\d\+-\.,]+) *')
+        date_gr, desc_gr, value_gr = r.match(s).groups()
 
-    def __init__(self, s):
-        r = re.compile(r' *(\d\d\d\d\.\d\d\.\d\d) *(.*) +([\d\+-\.,]+) *')
-        date_gr, desc_gr, count_gr = r.match(s).groups()
-        pass
+        if ';' not in desc_gr: desc_gr += ';'
+        value_gr = value_gr.replace(',', '.')
+
+        cats, tags = map(str.strip, desc_gr.split(';'))
+        self.cats = list(filter(None, map(str.strip, cats.split(','))))
+        self.tags = set(filter(None, map(str.strip, tags.split(','))))
+        self.time = datetime.date(*map(int, date_gr.split('.')))
+        self.value = eval(value_gr) if value_gr[0] == '+' else -eval(value_gr)
 
     def __str__(self):
-        return ''.format(self.category)
+        return "{} {};{} {}".format(self.time, ','.join(self.cats), ','.join(self.tags), -self.value)
 
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        # return self.__str__() == other.__str__()
+        return self.cats == other.cats and self.time == other.time and self.tags == other.tags and self.value == other.value
+
+
+
+class Bookkeeper:
+
+    def __init__(self):
+        self.value = 0
+
+    def process(self, e):
+        if e != Entry():
+            self.value += e.value
+
+    def __str__(self):
+        return "{}".format(self.value)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 if __name__ == "__main__":
-    print("Hello")
+    file_path = "/home/arwer/Notes/money.txt"
+    with open(file_path, "r") as ff:
+        text = ff.read()
+    start_index = text.find("\n\n\n")
+    if start_index != -1: text = text[start_index:]
+    bk = Bookkeeper()
+    for line in text.split("\n"):
+        e = Entry(line)
+        bk.process(e)
+    print(bk)
--- main_test.py
+++ main_test.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
 import unittest
-import main
-
+from main import *
+import datetime
 
 class TestSomething(unittest.TestCase):
 
-    def test_parse_line(self):
-        lines = [
-                "2015.07.09   food; tinned food 67+77+51+41+57+36+40",
-                "2015.07.08 household      34",
-                " 2015.07.08 misc;  +500 "
-        ]
-        for s in lines:
-            print(main.parse_line(s))
-        self.assertEqual(1, 1)
-        pass
+    def test_full_line(self):
+        line = "2015.07.09   food, milk; tinned food, tag2 67+77,3+51+41+57.0+36+40"
+        a = Entry()
+        a.cats = ["food", "milk"]
+        a.time = datetime.date(2015, 7, 9)
+        a.tags = set(["tinned food", "tag2"])
+        a.value = -eval("67+77.3+51+41+57+36+40")
+        self.assertEqual(Entry(line), a)
 
+    def test_positive_value(self):
+        line = "2015.07.09   food, milk; tinned food, tag2 +67+77+51+41+57+36+40"
+        a = Entry()
+        a.cats = ["food", "milk"]
+        a.time = datetime.date(2015, 7, 9)
+        a.tags = set(["tinned food", "tag2"])
+        a.value = eval("67+77+51+41+57+36+40")
+        self.assertEqual(Entry(line), a)
+
+    def test_shortest_line(self):
+        line = "2015.07.08 household      34"
+        a = Entry()
+        a.time = datetime.date(2015, 7, 8)
+        a.cats = ["household"]
+        a.tags = set()
+        a.value = -34
+        self.assertEqual(Entry(line), a)
+
+    def test_whitespace_or_empty_line(self):
+        self.assertEqual(Entry(""), Entry())
+        self.assertEqual(Entry("     "), Entry())
 
 
 if __name__ == "__main__":
