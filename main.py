b7a4e756a934bca399871b4f7895cb03adc0cce2
Artyom Veremeenko <arwer13@gmail.com>
2015-08-18 09:04:43 +0300

    A bit of entry filters.

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 import re
 import datetime
+import bisect
+from functools import total_ordering
+from dateutil import rrule
+from pprint import pprint
 
-
-
-def parse_line(s):
-    r = re.compile(r' *(\d\d\d\d\.\d\d\.\d\d) *([^;]+);?([^;]*) +([\d\+-\.,]+) *')
-    date_gr, cat_gr, tag_gr, count_gr = r.match(s).groups()
-    return date_gr, cat_gr, tag_gr, count_gr
-    # return None
-
-
+@total_ordering
 class Entry:
 
     def __init__(self, s=None):
@@ -35,7 +31,7 @@ class Entry:
         self.value = eval(value_gr) if value_gr[0] == '+' else -eval(value_gr)
 
     def __str__(self):
-        return "{} {};{} {}".format(self.time, ','.join(self.cats), ','.join(self.tags), -self.value)
+        return "{} {};{} {:.1f}".format(self.time, ','.join(self.cats), ','.join(self.tags), -self.value)
 
     def __repr__(self):
         return self.__str__()
@@ -44,22 +40,43 @@ class Entry:
         # return self.__str__() == other.__str__()
         return self.cats == other.cats and self.time == other.time and self.tags == other.tags and self.value == other.value
 
+    def __ge__(self, other):
+        return self.time >= other.time
+
 
 
 class Bookkeeper:
 
     def __init__(self):
-        self.value = 0
+        self.entries = []
 
     def process(self, e):
         if e != Entry():
-            self.value += e.value
+            bisect.insort_left(self.entries, e)
 
-    def __str__(self):
-        return "{}".format(self.value)
+    def get_total_value(self):
+        return sum([e.value for e in self.entries])
 
-    def __repr__(self):
-        return self.__str__()
+    def expenses_by_period(self, begin, end):
+        return list(filter(lambda x: begin <= x.time < end and x.value < 0, self.entries))
+
+    def expenses_by_cats(self, cats):
+        result = []
+        for e in self.entries:
+            if len(e.cats) >= len(cats) and all([e.cats[i] == cats[i] for i in range(len(cats))]):
+                result.append(e)
+        return result
+
+    def every_calendar_weak(self):
+        result = dict()
+        start_date = min(e.time for e in self.entries)
+        while start_date.weekday() != 0:
+            start_date -= datetime.timedelta(days=1)
+        for dt in rrule.rrule(rrule.WEEKLY, dtstart=start_date, until=datetime.date.today()):
+            begin, end = dt.date(), dt.date() + datetime.timedelta(days=7)
+            es = list(filter(lambda x: begin <= x.time < end and x.value < 0, self.entries))
+            result[(begin, end)] = -sum(a.value for a in es)
+        return result
 
 
 if __name__ == "__main__":
@@ -72,4 +89,7 @@ if __name__ == "__main__":
     for line in text.split("\n"):
         e = Entry(line)
         bk.process(e)
-    print(bk)
+    print(bk.get_total_value())
+    weakly = bk.every_calendar_weak()
+    pprint(weakly)
+    pprint(bk.expenses_by_cats(["food","water"]))
