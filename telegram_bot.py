3d4f857dff2e22fe4ec3e2f4bf57a7bf7f672ec9
Artyom Veremeenko <arwer13@gmail.com>
2017-07-30 15:52:21 +0300

    Fix telegram bot to handle multiple lines and commas.

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,6 +10,9 @@ import money
 import config
 
 
+dropbox_token = None
+"""Dropbox token, should be loaded at startup time."""
+
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
@@ -17,40 +20,30 @@ def start(bot, update):
     bot.sendMessage(update.message.chat_id, text='Hi!')
 
 
-def echo(bot, update):
-    logging.info("Got message: {}".format(update.message.text))
+def handle_record(bot, update):
+    text = update.message.text
+    logging.info("Got message: {}".format(text))
+    text_lines = text.splitlines()
+    record_date = update.message.date if update.message.forward_date is None\
+        else update.message.forward_date
 
-    def msg_to_line(msg):
+    def message_line_to_txt_line(msg):
         categories = []
         money = 0
-        for arg in filter(None, msg.text.split()):
+        for arg in filter(None, msg.replace(",", " ").split(" ")):
             try:
                 float(arg)
                 money = arg
             except ValueError:
                 categories.append(arg)
-        record_date = msg.date if msg.forward_date is None else msg.forward_date
         return "{} {} {}\n".format(record_date.strftime("%Y.%m.%d"), ", ".join(categories), money)
 
     data = dropbox_stuff.get_money_txt(dropbox_token)
     if data[-1] not in ["\r", "\n"]:
         data += "\n"
-    data += msg_to_line(update.message)
+    for line in text_lines:
+        data += message_line_to_txt_line(line)
     dropbox_stuff.set_money_txt(dropbox_token, data)
-
-    reply = "> " + update.message.text + "\n"
-    try:
-        bk = money.load_data(config)
-        model = money.make_model(bk, config.weekly_categories)
-    except Exception as e:
-        reply += str(e)
-    else:
-        num_categories = 4
-        monthly = model["monthly_by_categories"]
-        categories_info = ", ".join(["{}: {:.0f}".format(monthly[0][i], monthly[-1][i]["value"]) for i in range(4, 4 + num_categories + 1)])
-        reply += "Total: {:.0f}, spent this month: {:.0f} ({})".format(model["total_value"], monthly[-1][2], categories_info)
-
-    bot.sendMessage(update.message.chat_id, text=reply)
 
 
 def error(bot, update, error):
@@ -68,7 +61,7 @@ def start_bot(token):
     dp.add_handler(CommandHandler("start", start))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler([Filters.text], echo))
+    dp.add_handler(MessageHandler([Filters.text], handle_record))
 
     # log all errors
     dp.add_error_handler(error)
@@ -83,11 +76,13 @@ def start_bot(token):
     updater.idle()
 
 
-if __name__ == '__main__':
+def main():
+    global dropbox_token
     print("STARTED")
     log_file = os.path.splitext(os.path.basename(sys.argv[0]))[0] + ".log"
     logging.basicConfig(filename=log_file, level=logging.INFO,
-                        format="%(asctime)s %(levelname)s: %(message)s", datefmt="%Y.%m.%d %I:%M:%S")
+                        format="%(asctime)s %(levelname)s: %(message)s",
+                        datefmt="%Y.%m.%d %I:%M:%S")
     logging.info("Started")
     token = os.environ["MONEY_TXT_TELEGRAM_BOT_TOKEN"].strip('"')
     logging.info("Telegram token is found")
@@ -95,3 +90,7 @@ if __name__ == '__main__':
     logging.info("Dropbox token is found")
 
     start_bot(token)
+
+
+if __name__ == '__main__':
+    main()
